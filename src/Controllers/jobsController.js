5a8c04019372f29b7b0dbc3a19032a5a4dd57f71
brunopazitto <brunopazitto@outlook.com>
2020-06-09 20:01:25 -0300

    atualização

--- src/Controllers/jobsController.js
+++ src/Controllers/jobsController.js
@@ -5,9 +5,9 @@ const Jobs = mongoose.model('Jobs');
 module.exports = {
     async store(req,res){
         const response = await Jobs.create(req.body);
-        req.io.emit('newjob',response);
-       // res.json(response);
-       res.json().status(200);
+        //req.io.emit('newjob',response);
+        res.json(response);
+      // res.json().status(200);
     },
     async listJobs(req,res){
         const response = await Jobs.find();
